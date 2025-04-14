54204b2189ffc538f05683023f723e2b16d1e31e
inoUwU <inokawa.kouta@gmail.com>
2025-04-14 23:39:35 +0900

    feat: Implement Todo API with Gin and GORM

--- main.go
+++ main.go
@@ -1,14 +1,61 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type Todo struct {
+	ID        uint   `gorm:"primarykey" json:"id"`
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
+}
+
+var db *gorm.DB
+
+// DBを初期化する
+func initDB() {
+	db, err := gorm.Open(sqlite.Open("todo.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// テーブルが無ければ自動で生成される
+	db.AutoMigrate(&Todo{})
+}
+
+// TODO 作成
+func createTodo(c *gin.Context) {
+	var todo Todo
+	if err := c.Copy().ShouldBindJSON(&todo); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
+
+	db.Create(&todo)
+	c.JSON(http.StatusCreated, todo)
+}
+
+// Todo一覧を取得
+func getTodos(c *gin.Context) {
+	var todos []Todo
+	db.Find(&todos)
+	c.JSON(http.StatusOK, todos)
+}
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
 
-	})
+	initDB()
+
+	// 新規追加
+	r.POST("/todos", createTodo)
+
+	// 一覧取得
+	r.POST("/todos", getTodos)
+
+	// サーバー起動
 	r.Run(":8080")
 }
